16d29a71d59f2e4c80f85d5e153bcf2d8b7e431f
Floodinatorr <emirunalan44@gmail.com>
2022-10-23 08:40:00 +0300

    Logo ve Navbar eklendi.

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='avatars', blank=False, null=False, default='avatars/avatar.png')
+    phone = models.CharField(max_length=20, blank=False, null=False)
